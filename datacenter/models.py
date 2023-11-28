a740e0e14a9e1a3686f4622fd73d40c1445bba89
kiril <kirill2689@gmail.com>
2023-11-29 01:01:28 +0300

    Initial commit

---
+++ datacenter/models.py
@@ -0,0 +1,51 @@
+import datetime
+from django.db import models
+from django.utils.timezone import localtime
+
+
+def is_visit_long(duration, suspicious_time=None):
+    if not suspicious_time:
+        suspicious_time = datetime.timedelta(minutes=60)
+    return duration >= suspicious_time
+
+
+def format_duration(duration):
+    seconds = duration.total_seconds()
+    hours = int(seconds // 3600)
+    minutes = int((seconds % 3600) // 60)
+    return f"{hours}ч:{minutes}м"
+
+
+class Passcard(models.Model):
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now=True)
+    passcode = models.CharField(max_length=200, unique=True)
+    owner_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        if self.is_active:
+            return self.owner_name
+        return f'{self.owner_name} (inactive)'
+
+
+class Visit(models.Model):
+    created_at = models.DateTimeField(auto_now=True)
+    passcard = models.ForeignKey(Passcard, on_delete=models.CASCADE)
+    entered_at = models.DateTimeField()
+    leaved_at = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return '{user} entered at {entered} {leaved}'.format(
+            user=self.passcard.owner_name,
+            entered=self.entered_at,
+            leaved=(
+                f'leaved at {self.leaved_at}'
+                if self.leaved_at else 'not leaved'
+            )
+        )
+
+    def get_duration(self):
+        if self.leaved_at:
+            return localtime(self.leaved_at) - localtime(self.entered_at)
+        return localtime() - localtime(self.entered_at)
+
